09d9dd0f52fa072ae32cac8b1ab414b6c35ee237
Dasha Tikhonova <dashtikh@rambler.ru>
2021-02-27 12:44:14 +0300

    refactor files with pylint

--- conftest.py
+++ conftest.py
@@ -2,15 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', \
-                    action='store', \
-                    default='chrome', \
-                    help='Choose browser: chrome or firefox')
-    parser.addoption('--language', \
-                    action='store', \
-                    default='en', \
-                    help='Choose language: ru, en, ... (etc.)')
+    parser.addoption('--browser_name',
+                     action='store',
+                     default='chrome',
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language',
+                     action='store',
+                     default='en',
+                     help='Choose language: ru, en, ... (etc.)')
 
 
 @pytest.fixture(scope='function')
@@ -19,15 +20,16 @@ def browser(request):
     user_language = request.config.getoption('language')
     if browser_name == 'chrome':
         options = Options()
-        options.add_experimental_option('prefs', \
+        options.add_experimental_option('prefs',
                                         {'intl.accept_languages': user_language})
         print('\nstart chrome browser for test..')
         browser = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference('intl.accept_languages', user_language)
+        firefox_preference = webdriver.FirefoxProfile()
+        firefox_preference.set_preference(
+            'intl.accept_languages', user_language)
         print('\nstart firefox browser for test..')
-        browser = webdriver.Firefox(firefox_profile=fp)
+        browser = webdriver.Firefox(firefox_profile=firefox_preference)
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
