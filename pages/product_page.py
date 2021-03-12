bdff816298feaf47061d5398a982022c0b9d9bf9
Darya Tikhonova <dashtikh@rambler.ru>
2021-03-12 20:58:39 +0300

    getting rid of long lines

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,12 +13,15 @@ class ProductPage(BasePage):
         self.should_be_price_in_basket_equal_product_price()
 
     def should_be_product_name_in_message(self):
-        # Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+        # Сообщение о том, что товар добавлен в корзину.
+        # Название товара в сообщении должно совпадать с тем товаром,
+        # который вы действительно добавили.
         product_name_cart = self.browser.find_element(
             *ProductPageLocators.PRODUCT_NAME)
         product_name_message = self.browser.find_element(
             *ProductPageLocators.PRODUCT_NAME_MESSAGE)
-        assert product_name_cart.text == product_name_message.text, "Name of product in message is not equal name of product in cart"
+        assert product_name_cart.text == product_name_message.text, \
+            "Name of product in message is not equal name of product in cart"
 
     def should_be_price_in_basket_equal_product_price(self):
         # Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара.
@@ -26,4 +29,9 @@ class ProductPage(BasePage):
             *ProductPageLocators.PRODUCT_PRICE)
         product_price_message = self.browser.find_element(
             *ProductPageLocators.PRODUCT_PRICE_MESSAGE)
-        assert product_price_cart.text == product_price_message.text, "Price of product in message is not equal price of product in cart"
+        assert product_price_cart.text == product_price_message.text, \
+            "Price of product in message is not equal price of product in cart"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
 import pytest
 from pages.product_page import ProductPage
 
-@pytest.mark.parametrize('num', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+@pytest.mark.parametrize('num', [*range(1,7), \
+pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
 def test_guest_can_add_product_to_basket(browser, num):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
     page = ProductPage(browser, link)
