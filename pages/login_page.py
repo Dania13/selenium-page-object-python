995fd837b274cd01500d21db6c8e14136095fdba
Darya Tikhonova <dashtikh@rambler.ru>
2021-03-13 23:54:13 +0300

    add tests for autorized users

--- pages/login_page.py
+++ pages/login_page.py
@@ -21,3 +21,17 @@ class LoginPage(BasePage):
         # проверка, что есть форма регистрации на странице
         assert self.is_element_present(
             *LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(
+            *LoginPageLocators.EMAIL_REGISTER)
+        password_field = self.browser.find_element(
+            *LoginPageLocators.PASSWORD_REGISTER)
+        confirm_password_field = self.browser.find_element(
+            *LoginPageLocators.CONFIRM_PASSWORD_REGISTER)
+        register_btn = self.browser.find_element(
+            *LoginPageLocators.REGISTER_BUTTON)
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        confirm_password_field.send_keys(password)
+        register_btn.click()
